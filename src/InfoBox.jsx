369c2e630194a3808168f5c78b91ed4343213eae
Aditya Saxena <adityasaxena0050@gmail.com>
2024-01-11 18:44:49 +0530

    Add Project Files

---
+++ src/InfoBox.jsx
@@ -0,0 +1,41 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+export default function InfoBox({info}){
+    const INIT_URL="https://images.unsplash.com/photo-1572687413625-cb2c4d9c4d32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D"
+    const HOT_URL="https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8SE9UJTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const COLD_URL="https://images.unsplash.com/photo-1425100599170-85ec4f00a6ee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const RAIN_URL="https://images.unsplash.com/photo-1618557703025-7ec58c83581a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFpbiUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    return (
+        <div className="InfoBox">
+            <div className="cardContainer">
+                <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={info.humudity>80 ? RAIN_URL : info.temp>15 ? HOT_URL : COLD_URL}
+                    title="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    {info.city}{info.humudity>80 ? (<ThunderstormIcon/>) : info.temp>15 ? (<WbSunnyIcon/>) : (<AcUnitIcon/>)}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary"component={"span"}>
+                        <p>Temperature = {info.temp}&deg;C</p>
+                        <p>humidity = {info.humidity}</p>
+                        <p>Min Temp = {info.tempMin}&deg;C</p>
+                        <p>Max Temp = {info.tempMax}&deg;C</p>
+                        <p>
+                            The weather can be described as <i>{info.weather}</i> and feels like {info.feelslike}&deg;C
+                        </p>
+                    </Typography>
+                </CardContent>
+                </Card>
+            </div>
+        </div>
+      );
+}
